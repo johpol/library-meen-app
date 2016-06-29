23354cf39fa3c4f2bcb9542ad0c99b5687a2ab31
johpol <jp7358@gmail.com>
2016-06-28 20:26:10 -0400

    Added book route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/LibraryApp'); 
+
+var bk = mongoose.model('Book', {
+    text : String
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/book', function(req, res) {
+    bk.find(function(err, book) {
+        if(err)
+            res.send(err)
+        res.json(book);
+    });
 });
 
 module.exports = router;
