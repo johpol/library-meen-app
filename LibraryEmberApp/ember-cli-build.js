c845cb5d14dd52423713a22893e9b32b293e8921
johpol <jp7358@gmail.com>
2016-10-18 19:51:22 -0400

    Added Ember bootstrap addon Pulling in optional bootstrap theme Ported over MEAN Jade template to handlebars

--- LibraryEmberApp/ember-cli-build.js
+++ LibraryEmberApp/ember-cli-build.js
@@ -2,9 +2,12 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    'ember-bootstrap': {
+      'importBootstrapTheme': true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
