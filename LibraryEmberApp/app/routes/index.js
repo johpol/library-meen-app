4a3ded517914e3d1efb9b7976b72f9146601900e
johpol <jp7358@gmail.com>
2016-10-29 20:21:42 -0400

    Model hook with ember ajax get request Each handlebar helper added to template that displays the collection of books

--- LibraryEmberApp/app/routes/index.js
+++ LibraryEmberApp/app/routes/index.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    ajax: Ember.inject.service(),
+
+    model() {
+        let mod;
+        mod = this.get('ajax').request('/book', {
+        method: 'GET'
+      });
+      return mod;
+    }
 });
