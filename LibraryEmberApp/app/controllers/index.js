95a245548fdf776f5bb8138c4582df2f71d70900
johpol <jp7358@gmail.com>
2016-11-26 20:43:51 -0500

    Added deleteIsbn action to Index controller Set isbn to an empty string so it clears the ISBN after submission Added action helper to template

--- LibraryEmberApp/app/controllers/index.js
+++ LibraryEmberApp/app/controllers/index.js
@@ -4,13 +4,24 @@ export default Ember.Controller.extend({
     ajax: Ember.inject.service(),
 
     actions: {
-        postIsbn(isbn) {
+        postIsbn() {
+            let isbn = Ember.get(this, 'isbn');
             console.log(isbn);
             return this.get('ajax').request('/book', {
                 method: 'POST',
                 data: {
                     text: isbn
                 }
+            }).then((data) => {
+                Ember.set(this, 'isbn', '');
+                Ember.set(this, 'model', data);
+            });
+        },
+
+        deleteIsbn(bookID) {
+            let url = '/book/' + bookID;
+            return this.get('ajax').request(url, {
+                method: 'DELETE'
             }).then((data) => {
                 Ember.set(this, 'model', data);
             });
