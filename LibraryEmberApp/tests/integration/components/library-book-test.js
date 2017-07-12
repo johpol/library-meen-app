90ac54b334178212e3175e0c7e525ecaa0e3f0e5
johpol <johpol@users.noreply.github.com>
2017-07-11 20:35:20 -0400

    tests for book model and library-book component

--- LibraryEmberApp/tests/integration/components/library-book-test.js
+++ LibraryEmberApp/tests/integration/components/library-book-test.js
@@ -1,5 +1,18 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
+
+const {$} = Ember;
+
+const model =[
+  {
+    title: "Some Title",
+    author: "Some Author",
+    publisher: "Some Publisher",
+    isbn: "1234567890",
+    subject: "Some Subject",
+  }
+]
 
 moduleForComponent('library-book', 'Integration | Component | library book', {
   integration: true
@@ -10,16 +23,18 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{library-book}}`);
+  this.set('model', model);
+  this.set('deleteIsbn', () => assert.ok(true));
+  this.set('postIsbn', () => assert.ok(true));
+  this.render(hbs`{{library-book model=model postIsbn=(action postIsbn) deleteIsbn=(action deleteIsbn)}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  const tableCells = $('table')["0"].children[1].children["0"].cells;
 
-  // Template block usage:
-  this.render(hbs`
-    {{#library-book}}
-      template block text
-    {{/library-book}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(tableCells.length, 6);
+  assert.equal(tableCells[0].innerText, 'Some Title');
+  assert.equal(tableCells[1].innerText, 'Some Author');
+  assert.equal(tableCells[2].innerText, 'Some Publisher');
+  assert.equal(tableCells[3].innerText, 'Some Subject');
+  assert.equal(tableCells[4].innerText, '1234567890');
+  assert.equal(tableCells[5].innerText, 'Remove');
 });
--- LibraryEmberApp/tests/unit/models/book-test.js
+++ LibraryEmberApp/tests/unit/models/book-test.js
@@ -1,12 +1,30 @@
 import { moduleForModel, test } from 'ember-qunit';
 
+const bookModel = {
+    _id: 'someID',
+    title: 'someTitle',
+    author: 'someAuthor',
+    publisher: 'somePublisher',
+    isbn: 'isbn',
+    tempIsbn: 'tempIsbn',
+    subject: 'someSubject'
+};
+
 moduleForModel('book', 'Unit | Model | book', {
   // Specify the other units that are required for this test.
   needs: []
 });
 
 test('it exists', function(assert) {
-  let model = this.subject();
+  let model = this.subject(bookModel);
   // let store = this.store();
   assert.ok(!!model);
+
+  assert.equal(model.get('_id'), 'someID');
+  assert.equal(model.get('title'), 'someTitle');
+  assert.equal(model.get('author'), 'someAuthor');
+  assert.equal(model.get('publisher'), 'somePublisher');
+  assert.equal(model.get('isbn'), 'isbn');
+  assert.equal(model.get('tempIsbn'), 'tempIsbn');
+  assert.equal(model.get('subject'), 'someSubject');
 });
